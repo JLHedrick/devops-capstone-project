760d4e4cfdf75b5d1303a93f1c35c8f8b59ff056
JLHedrick <hedrick@us.ibm.com>
2024-06-10 19:56:54 -0400

    testing

--- service/routes.py
+++ service/routes.py
@@ -87,10 +87,7 @@ def create_accounts():
         return account.serialize(), status.HTTP_200_OK
 
 
-    def test_get_account_not_found(self):
-        """It should not Read an Account that is not found"""
-        resp = self.client.get(f"{BASE_URL}/0")
-        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+    
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
