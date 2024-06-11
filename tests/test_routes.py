f9ff947001aad6ad0c4d3c143a6db3a5da17c4de
JLHedrick <hedrick@us.ibm.com>
2024-06-11 18:19:23 -0400

    Added CORS headers

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -22,6 +22,7 @@ DATABASE_URI = os.getenv(
 BASE_URL = "/accounts"
 HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 
+
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
@@ -189,10 +190,8 @@ class TestAccountService(TestCase):
             self.assertEqual(response.headers.get(key), value)
 
     def test_cors_security(self):
-    """It should return a CORS header"""
-    response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
-    self.assertEqual(response.status_code, status.HTTP_200_OK)
-    # Check for the CORS header
-    self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
-
-
+        """It should return a CORS header"""
+        response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # Check for the CORS header
+        self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
